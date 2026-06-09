a7eabb8cd136193d13e435afba9ad8d2f30975dd
bonyhenry8-prog <bonyhenry8@gmail.com>
2026-06-09 12:48:01 -0500

    Actualización: Cambios en diseño y datos de fútbol

--- datos.js
+++ datos.js
@@ -9,13 +9,13 @@ const jorndasDeportivasData = {
                     equipos: [
                         { id: "m_leyendas_2004", nombre: "LEYENDAS 2004", pj: 5, gd: -7, pts: 7 },
                         { id: "m_la_colacion_sub40", nombre: "CMC LA COLACION SUB 40", pj: 5, gd: -8, pts: 4 },
-                        { id: "m_escolta_metro", nombre: "ESCOLTA METROPOLITANA", pj: 5, gd: +11, pts: 12 },
-                        { id: "m_gats_calcibon", nombre: "GAT-S MÁSTER (LOS CALCIBON)", pj: 5, gd: +14, pts: 15 },
+                        { id: "m_escolta_metro", nombre: "ESCOLTA METROPOLITANA", pj: 6, gd: +20, pts: 15 },
+                        { id: "m_gats_calcibon", nombre: "GAT-S MÁSTER (LOS CALCIBON)", pj: 6, gd: +18, pts: 18 },
                         { id: "m_transportes", nombre: "TRANSPORTES", pj: 5, gd: +15, pts: 13 },
-                        { id: "m_los_12_mas_1", nombre: "LOS 12+1", pj: 5, gd: -9, pts: 4 },
-                        { id: "m_nottingham_miedo", nombre: "ATLETICO IESS NOTTINGHAN-MIEDO", pj: 4, gd: +3, pts: 4 },
+                        { id: "m_los_12_mas_1", nombre: "LOS 12+1", pj: 6, gd: -11, pts: 4 },
+                        { id: "m_nottingham_miedo", nombre: "ATLETICO IESS NOTTINGHAN-MIEDO", pj: 5, gd: +5, pts: 7 },
                         { id: "m_transportes_caf", nombre: "TRANSPORTES CAF", pj: 6, gd: -7, pts: 4 },
-                        { id: "m_metros_galacticos", nombre: "LOS METROS GALÁCTICOS", pj: 4, gd: -11, pts: 0 }
+                        { id: "m_metros_galacticos", nombre: "LOS METROS GALÁCTICOS", pj: 5, gd: -20, pts: 0 }
                     ]
                 },
                 senior: {
@@ -25,16 +25,16 @@ const jorndasDeportivasData = {
                         { id: "s_a_administrativos", nombre: "ADMINISTRATIVOS", pj: 3, gd: +8, pts: 7 },
                         { id: "s_a_fila_1", nombre: "FILA 1", pj: 3, gd: +13, pts: 9 },
                         { id: "s_a_fila_2", nombre: "FILA 2", pj: 4, gd: 5, pts: 7 },
-                        { id: "s_a_gats_lobos", nombre: "GAT-S A ( LOS LOBOS)", pj: 3, gd: -6, pts: 1 },
-                        { id: "s_a_gats_barnys", nombre: "GAT-S B (LOS BARNYS)", pj: 4, gd: 6, pts: 7 },
+                        { id: "s_a_gats_lobos", nombre: "GAT-S A ( LOS LOBOS)", pj: 4, gd: -10, pts: 1 },
+                        { id: "s_a_gats_barnys", nombre: "GAT-S B (LOS BARNYS)", pj: 5, gd: 4, pts: 10 },
                         { id: "s_a_cmc_ecu911", nombre: "CMC Y ECU 911", pj: 4, gd: -6, pts: 2 }
                     ],
                     grupoB: [
                         { id: "s_b_gam", nombre: "GAM", pj: 3, gd: 3, pts: 4 },
                         { id: "s_b_transporte_publico", nombre: "TRANSPORTE PÚBLICO", pj: 3, gd: 2, pts: 5 },
-                        { id: "s_b_mala_fama_united", nombre: "MALA FAMA UNITED", pj: 3, gd: +3, pts: 7 },
+                        { id: "s_b_mala_fama_united", nombre: "MALA FAMA UNITED", pj: 4, gd: -9, pts: 7 },
                         { id: "s_b_rescate", nombre: "RESCATE", pj: 3, gd: -4, pts: 3 },
-                        { id: "s_b_turismo", nombre: "TURISMO", pj: 3, gd: +19, pts: 4 },
+                        { id: "s_b_turismo", nombre: "TURISMO", pj: 4, gd: +31, pts: 7 },
                         { id: "s_b_caballeria_fc", nombre: "CABALLERIA FC", pj: 3, gd: -13, pts: 1 }
                     ]
                 },
@@ -42,9 +42,9 @@ const jorndasDeportivasData = {
                     nombre: "Femenino",
                     equipos: [
                         { id: "f_manuela_saenz", nombre: "U.O.Z MANUELA SÁENZ", pj: 3, gd: +5, pts: 5 },
-                        { id: "f_transporte_publico", nombre: "TRANSPORTE PÚBLICO", pj: 2, gd: -3, pts: 1 },
+                        { id: "f_transporte_publico", nombre: "TRANSPORTE PÚBLICO", pj: 3, gd: +1, pts: 4 },
                         { id: "f_gats_femenino", nombre: "GAT-S FEMENINO", pj: 3, gd: +12, pts: 7 },
-                        { id: "f_administrativos", nombre: "ADMINISTRATIVOS", pj: 2, gd: -14, pts: 0 }
+                        { id: "f_administrativos", nombre: "ADMINISTRATIVOS", pj: 3, gd: -18, pts: 0 }
                     ]
                 }
             }
