dc41360766a8eb33682a78af0888d37e87c5ccb8
bonyhenry8-prog <bonyhenry8@gmail.com>
2026-06-09 09:15:32 -0500

    Actualización completa: Diseño de goleadores y tablas

--- cronograma_datos.js
+++ cronograma_datos.js
@@ -1,52 +1,48 @@
 const cronogramaData = {
-  "01": {
+  "08": {
     "Microfútbol": [
-      { categoria: "MASTER", equipoA: "GAT-S", equipoB: "LOS METROS GALACTICOS", hora: "07:00" },
-      { categoria: "MASTER", equipoA: "LOS 12+1", equipoB: "TRANSPORTES", hora: "08:30" },
-      { categoria: "MASTER", equipoA: "NOTTINGHAM-MIEDO ATLÉTICO IESS", equipoB: "LEYENDAS 2004", hora: "10:00" },
-      { categoria: "MASTER", equipoA: "ESCOLTA METROPOLITANA", equipoB: "TRANSPORTES CAF", hora: "11:30" },
-      { categoria: "SENIOR", equipoA: "GAT-S 2", equipoB: "CMC Y ECU 911", hora: "13:00" },
-      { categoria: "SENIOR", equipoA: "FILA 2", equipoB: "GAT-S 1", hora: "14:30" }
+      { categoria: "SENIOR", equipoA: "GAT-S 1", equipoB: "GAT-S 2", hora: "07:00" },
+      { categoria: "SENIOR", equipoA: "MALA FAMA UNITED", equipoB: "TURISMO", hora: "08:30" },
+      { categoria: "MASTER", equipoA: "ESCOLTA METROPOLITANA", equipoB: "LOS METROS GALACTICOS", hora: "10:00" },
+      { categoria: "MASTER", equipoA: "LOS 12+1", equipoB: "NOTTINGHAM-MIEDO ATLÉTICO IESS", hora: "11:30" },
+      { categoria: "MASTER", equipoA: "GAT-S", equipoB: "CMC LA COLISIÓN SUB 40", hora: "13:00" },
+      { categoria: "FEMENINO", equipoA: "TRANSPORTE PÚBLICO", equipoB: "ADMINISTRATIVAS", hora: "14:30" }
     ]
   },
-  "02": {
-    "Microfútbol": [
-      { categoria: "SENIOR", equipoA: "MALA FAMA UNITED", equipoB: "CABALLERIA FC", hora: "07:00" },
-      { categoria: "SENIOR", equipoA: "FILA 1", equipoB: "GAT-S 1", hora: "08:30" }
-    ],
+  "09": {
     "Ecuavoley": [
-      { categoria: "S/C", equipoA: "TRANSPORTE PÚBLICO", equipoB: "SPARTANS", hora: "08:30" },
-      { categoria: "S/C", equipoA: "GALLOS", equipoB: "SOLO PANAS", hora: "10:30" },
-      { categoria: "S/C", equipoA: "LOS ALCONES", equipoB: "UNIDAD TIC", hora: "12:30" }
-    ]
-  },
-  "03": {
-    "Microfútbol": [
-      { categoria: "FEMENINO", equipoA: "TRANSPORTE PÚBLICO", equipoB: "ADMINISTRATIVAS", hora: "07:00" },
-      { categoria: "FEMENINO", equipoA: "GAT-S", equipoB: "CENTRO HISTORICO", hora: "08:30" },
-      { categoria: "SENIOR", equipoA: "TRANSPORTE PÚBLICO", equipoB: "TURISMO", hora: "10:00" },
-      { categoria: "SENIOR", equipoA: "GAM", equipoB: "RESCATE", hora: "11:30" },
-      { categoria: "SENIOR", equipoA: "K-NINOS", equipoB: "FILA 2", hora: "13:00" }
-    ]
-  },
-  "04": {
-    "Microfútbol": [
-      { categoria: "MASTER", equipoA: "TRANSPORTES CAF", equipoB: "CMC LA COLISIÓN SUB 40", hora: "07:00" },
-      { categoria: "SENIOR", equipoA: "ADMINISTRATIVAS", equipoB: "GAT-S 2", hora: "08:30" },
-      { categoria: "MASTER", equipoA: "GAT-S", equipoB: "NOTTINGHAM-MIEDO ATLÉTICO IESS", hora: "10:00" },
-      { categoria: "MASTER", equipoA: "LOS 12+1", equipoB: "LEYENDAS 2004", hora: "11:30" }
+      { categoria: "S/C", equipoA: "TITANES DEL ECUAVOLEY", equipoB: "BATE Y SALE", hora: "09:00" },
+      { categoria: "S/C", equipoA: "SOLO PANAS", equipoB: "TAUROS", hora: "11:00" },
+      { categoria: "S/C", equipoA: "LOS ALCONES", equipoB: "SPARTANS", hora: "13:00" },
+      { categoria: "S/C", equipoA: "TRANSPORTE PÚBLICO", equipoB: "UNIDAD TIC", hora: "15:00" }
     ],
     "Basket": [
-      { categoria: "ABIERTO", equipoA: "MASTERS MIX", equipoB: "NSJ", hora: "09:00" },
-      { categoria: "ABIERTO", equipoA: "METRITOS SPACE JAM", equipoB: "ALFA METRITOS", hora: "10:00" }
+      { categoria: "ABIERTO", equipoA: "ALFA METRITOS", equipoB: "METRITOS SPACE JAM", hora: "09:00" },
+      { categoria: "ABIERTO", equipoA: "NSJ", equipoB: "MASTERS MIX", hora: "10:00" }
     ]
   },
-  "05": {
+  "10": {
+    "Microfútbol": [
+      { categoria: "SENIOR", equipoA: "TRANSPORTE PÚBLICO", equipoB: "RESCATE", hora: "07:00" },
+      { categoria: "SENIOR", equipoA: "FILA 1", equipoB: "FILA 2", hora: "08:30" },
+      { categoria: "SENIOR", equipoA: "K-NINOS", equipoB: "CMC Y ECU 911", hora: "10:00" },
+      { categoria: "FEMENINO", equipoA: "CENTRO HISTORICO", equipoB: "GAT-S", hora: "11:30" },
+      { categoria: "FEMENINO", equipoA: "ADMINISTRATIVAS", equipoB: "TRANSPORTE PÚBLICO", hora: "13:00" },
+      { categoria: "SENIOR", equipoA: "GAM", equipoB: "CABALLERIA FC", hora: "14:30" }
+    ]
+  },
+  "11": {
+    "Microfútbol": [
+      { categoria: "MASTER", equipoA: "ESCOLTA METROPOLITANA", equipoB: "TRANSPORTES", hora: "07:00" },
+      { categoria: "SENIOR", equipoA: "ADMINISTRATIVOS", equipoB: "GAT-S 1", hora: "08:30" },
+      { categoria: "MASTER", equipoA: "NOTTINGHAM-MIEDO ATLÉTICO IESS", equipoB: "LOS METROS GALACTICOS", hora: "10:00" },
+      { categoria: "MASTER", equipoA: "TRANSPORTES CAF", equipoB: "LEYENDAS 2004", hora: "11:30" },
+      { categoria: "MASTER", equipoA: "LOS 12+1", equipoB: "CMC LA COLISIÓN SUB 40", hora: "13:00" }
+    ],
     "Ecuavoley": [
-      { categoria: "S/C", equipoA: "TITANES DEL ECUAVOLEY", equipoB: "LOS ALCONES", hora: "08:30" },
-      { categoria: "S/C", equipoA: "TRANSPORTE PÚBLICO", equipoB: "BATE Y SALE", hora: "10:30" },
-      { categoria: "S/C", equipoA: "GALLOS", equipoB: "TAUROS", hora: "12:30" },
-      { categoria: "S/C", equipoA: "SOLO PANAS", equipoB: "SPARTANS", hora: "14:30" }
+      { categoria: "S/C", equipoA: "GALLOS", equipoB: "UNIDAD TIC", hora: "08:30" },
+      { categoria: "S/C", equipoA: "TRANSPORTE PÚBLICO", equipoB: "SOLO PANAS", hora: "10:30" },
+      { categoria: "S/C", equipoA: "TITANES DEL ECUAVOLEY", equipoB: "TAUROS", hora: "12:30" }
     ]
   }
 };
